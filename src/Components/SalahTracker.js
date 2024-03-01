bd81800b43924a8037f57c7ef46c310e2f68ba40
berat-552 <berat.d7599@gmail.com>
2024-03-01 23:52:55 +0000

    capitalise country and city names

--- src/Components/SalahTracker.js
+++ src/Components/SalahTracker.js
@@ -1,26 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Chart } from 'primereact/chart';
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Chart } from "primereact/chart";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 import "../App.css";
 
 const SalahTracker = () => {
-  const [city, setCity] = useState('');
-  const [country, setCountry] = useState('');
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
   const [salahTimes, setSalahTimes] = useState(null);
   const [fetching, setFetching] = useState(false);
 
   const fetchData = async () => {
     setFetching(true);
-    const response = await axios.get('http://api.aladhan.com/v1/timingsByCity', {
-      params: {
-        city: city,
-        country: country,
-        method: '2'
+    const response = await axios.get(
+      "http://api.aladhan.com/v1/timingsByCity",
+      {
+        params: {
+          city: city,
+          country: country,
+          method: "2",
+        },
       }
-    });
+    );
     setSalahTimes(response.data.data.timings);
     setFetching(false);
   };
@@ -33,35 +36,35 @@ const SalahTracker = () => {
 
   const convertSalahTimesToChartData = (salahTimes) => {
     return {
-      labels: ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha'],
+      labels: ["Fajr", "Dhuhr", "Asr", "Maghrib", "Isha"],
       datasets: [
         {
           data: [
-            salahTimes.Fajr.split(':').reduce((acc, val) => 60 * acc + +val),
-            salahTimes.Dhuhr.split(':').reduce((acc, val) => 60 * acc + +val),
-            salahTimes.Asr.split(':').reduce((acc, val) => 60 * acc + +val),
-            salahTimes.Maghrib.split(':').reduce((acc, val) => 60 * acc + +val),
-            salahTimes.Isha.split(':').reduce((acc, val) => 60 * acc + +val)
+            salahTimes.Fajr.split(":").reduce((acc, val) => 60 * acc + +val),
+            salahTimes.Dhuhr.split(":").reduce((acc, val) => 60 * acc + +val),
+            salahTimes.Asr.split(":").reduce((acc, val) => 60 * acc + +val),
+            salahTimes.Maghrib.split(":").reduce((acc, val) => 60 * acc + +val),
+            salahTimes.Isha.split(":").reduce((acc, val) => 60 * acc + +val),
           ],
           backgroundColor: [
-            'rgba(255, 165, 0, 0.6)', // Fajr
-            'rgba(255, 140, 0, 0.6)', // Dhuhr
-            'rgba(255, 127, 80, 0.6)', // Asr
-            'rgba(255, 99, 71, 0.6)', // Maghrib
-            'rgba(255, 69, 0, 0.6)' // Isha
+            "rgba(255, 165, 0, 0.6)", // Fajr
+            "rgba(255, 140, 0, 0.6)", // Dhuhr
+            "rgba(255, 127, 80, 0.6)", // Asr
+            "rgba(255, 99, 71, 0.6)", // Maghrib
+            "rgba(255, 69, 0, 0.6)", // Isha
           ],
           hoverBackgroundColor: [
-            'rgba(255, 165, 0, 1)', // Fajr
-            'rgba(255, 140, 0, 1)', // Dhuhr
-            'rgba(255, 127, 80, 1)', // Asr
-            'rgba(255, 99, 71, 1)', // Maghrib
-            'rgba(255, 69, 0, 1)' // Isha
-          ]
-        }
-      ]
+            "rgba(255, 165, 0, 1)", // Fajr
+            "rgba(255, 140, 0, 1)", // Dhuhr
+            "rgba(255, 127, 80, 1)", // Asr
+            "rgba(255, 99, 71, 1)", // Maghrib
+            "rgba(255, 69, 0, 1)", // Isha
+          ],
+        },
+      ],
     };
   };
-  
+
   return (
     <div className="salah-tracker">
       <input
@@ -78,14 +81,18 @@ const SalahTracker = () => {
         value={country}
         onChange={(e) => setCountry(e.target.value)}
       />
-      <button className="enter-button" onClick={handleButtonClick} disabled={fetching}>
-        {fetching ? 'Fetching...' : 'Enter'}
+      <button
+        className="enter-button"
+        onClick={handleButtonClick}
+        disabled={fetching}
+      >
+        {fetching ? "Fetching..." : "Enter"}
       </button>
       {salahTimes && (
         <div className="salah-times">
           <h2>Salah Times for Today</h2>
-          <p>City: {city}</p>
-          <p>Country: {country}</p>
+          <p>City: {city.charAt(0).toUpperCase() + city.slice(1)}</p>
+          <p>Country: {country.charAt(0).toUpperCase() + country.slice(1)}</p>
           <ul>
             <li>Fajr: {salahTimes.Fajr}</li>
             <li>Dhuhr: {salahTimes.Dhuhr}</li>
@@ -101,7 +108,3 @@ const SalahTracker = () => {
 };
 
 export default SalahTracker;
-
-
-
-
